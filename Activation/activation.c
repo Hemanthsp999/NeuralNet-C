04d059d57f4549d25f450d14502ecb6db1f55472
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-04-23 18:47:52 +0530

    -refactored file structure.

--- Activation/activation.c
+++ Activation/activation.c
@@ -2,3 +2,7 @@
 #include <math.h>
 
 double SigmoidFunc(double x) { return 1 / (1 + exp(-x)); }
+
+double tan_h(double x) { return (sin(x) / cos(x)); }
+
+double relu(double x) { return max(0, x); }
--- Activation/activation.h
+++ Activation/activation.h
@@ -1,6 +1,11 @@
 #ifndef Activation_
 #define Activation_
 
+const int zero = 0;
+#define max(zero, x) ((x > zero) ? x : zero)
+
 double SigmoidFunc(double x);
+double tan_h(double x);
+double relu(double);
 
 #endif
