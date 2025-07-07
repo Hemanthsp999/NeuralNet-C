9ed4de086932fab1e39e01298fbe6b925c343eb8
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-07-07 19:47:51 +0530

    -working on clearing memory.

--- File/memory.c
+++ File/memory.c
@@ -1,78 +1,55 @@
 #include "file.h"
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /* Deallocate the memory */
-void free_dataset(dataset_handler *handler) {
-        if (!handler)
-                return;
-
-        // Free training data
-        if (handler->x_train) {
-                for (size_t i = 0; i < handler->train_samples; i++) {
-                        free(handler->x_train[i]);
-                        handler->x_train[i] = NULL;
-                }
-                free(handler->x_train);
-                handler->x_train = NULL;
+void free_dataset(dataset_handler *dataset) {
+        if (!dataset) {
+                fprintf(stderr, "Please pass Resource to Free.\n");
+                assert(dataset);
         }
 
-        if (handler->y_train) {
-                for (size_t i = 0; i < handler->train_samples; i++) {
-                        free(handler->y_train[i]); // strdup
-                        handler->y_train[i] = NULL;
+        if (dataset->x_val && dataset->y_val) {
+                for (size_t i = 0; i < dataset->val_size; ++i) {
+                        free(dataset->x_val[i]);
+                        dataset->x_val[i] = NULL;
+                        free(dataset->y_val[i]);
+                        dataset->y_val[i] = NULL;
                 }
-                free(handler->y_train);
-                handler->y_train = NULL;
+                free(dataset->y_val);
+                free(dataset->x_val);
         }
 
-        // Free testing data
-        if (handler->x_test) {
-                for (size_t i = 0; i < handler->test_samples; i++) {
-                        free(handler->x_test[i]);
-                        handler->x_test[i] = NULL;
+        if (dataset->x_test && dataset->y_test) {
+                for (size_t i = 0; i < dataset->test_size; i++) {
+                        free(dataset->x_test[i]);
+                        dataset->x_test[i] = NULL;
+                        free(dataset->y_test[i]);
+                        dataset->y_test[i] = NULL;
                 }
-                free(handler->x_test);
-                handler->x_test = NULL;
+                free(dataset->y_test);
+                free(dataset->x_test);
         }
 
-        if (handler->y_test) {
-                for (size_t i = 0; i < handler->test_samples; i++) {
-                        free(handler->y_test[i]); // strdup
-                        handler->y_test[i] = NULL;
-                }
-                free(handler->y_test);
-                handler->y_test = NULL;
+        for (size_t i = 0; i < dataset->train_size; i++) {
+                free(dataset->x_train[i]);
+                dataset->x_train[i] = NULL;
+                free(dataset->y_train[i]);
+                dataset->y_train[i] = NULL;
         }
+        free(dataset->x_train);
+        free(dataset->y_train);
 
-        // Free entire dataset X and Y if present
-        if (handler->X) {
-                for (size_t i = 0; i < handler->samples; i++) {
-                        if (handler->X[i]) {
-                                free(handler->X[i]);
-                                handler->X[i] = NULL;
-                        }
-                }
-                free(handler->X);
-                handler->X = NULL;
+        for (size_t i = 0; i < dataset->samples; i++) {
+                free(dataset->X[i]);
+                dataset->X[i] = NULL;
         }
+        free(dataset->X);
+        dataset->X = NULL;
+        free(dataset->Y);
+        dataset->Y = NULL;
 
-        if (handler->Y) {
-                for (size_t i = 0; i < handler->samples; i++) {
-                        if (handler->Y[i]) {
-                                free(handler->Y[i]); // strdup
-                                handler->Y[i] = NULL;
-                        }
-                }
-                free(handler->Y);
-                handler->Y = NULL;
-        }
-        free(handler->x_train);
-        free(handler->y_train);
-        free(handler->x_test);
-        free(handler->y_test);
-        free(handler->X);
-        free(handler->Y);
-        free(handler);
-        handler = NULL;
+        free(dataset);
+        dataset = NULL;
 }
